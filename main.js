9c207b29631fae37004ae56f216c5fea55c84599
Shiranka <shiranka.miskin@gmail.com>
2015-10-01 15:39:43 -0700

    going to test resize flash

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 1360, height: 800});
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  setTimeout(() => mainWindow.setSize(100, 100), 1000);
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
--- src/entry.js
+++ src/entry.js
@@ -2,8 +2,6 @@
 
 import React from "react";
 
-func();
-
 React.render(
   <div className="myDiv">Hello World</div>,
   document.getElementById('content')
